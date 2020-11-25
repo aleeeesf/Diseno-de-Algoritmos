6fd925dc03e0f5e13e2ff358a2dbeeee0c54b0f0
aleeeesf <alejandroserfer@gmail.com>
2020-11-25 20:36:18 +0100

    P2

--- SERRANO_FERNANDEZ_ALEJANDRO/p2/DefensesSelection.cpp
+++ SERRANO_FERNANDEZ_ALEJANDRO/p2/DefensesSelection.cpp
@@ -10,62 +10,94 @@
 
 using namespace Asedio;
 
-double asignar_valor()
-{
-
+double asignar_valor(const Defense& d){
+    double x= d.range/d.damage;
+    return x;
 }
+
 
 void DEF_LIB_EXPORTED selectDefenses(std::list<Defense*> defenses, unsigned int ases, std::list<int> &selectedIDs
             , float mapWidth, float mapHeight, std::list<Object*> obstacles) {
 
 
 	int N = defenses.size(), i = 0;	//Capacidad total de defensas
-	double p[N], v[N], f[N][ases];
+	int p[N];
+	double v[N], f[N][ases+1];
 	
-	for(std::list<Defense*>::iterator i = defenses.begin(); i != defenses.end(); i++)
+
+	//Asignar valores
+	for(std::list<Defense*>::iterator it = defenses.begin(); it != defenses.end(); it++)
 	{
-		v[i] = asignar_valor();
-		p[i] = (*i)->cost;
+		v[i] = asignar_valor(*(*it));
+		p[i] = (*it)->cost;
 		i++;
 	}
 
-	for(int j = 0; i < ases; j++)
+
+
+	//Algoritmo que determina el máximo beneficio a obtener
+	for(int j = 0; j <= ases; j++)
 	{
-		if(j < p[1])
-			f[1][j] = 0;
+		if(j < p[0])
+			f[0][j] = 0;
 		else
-			f[1][j] = v[1];
+			f[0][j] = v[0];
 	}
 
 
 
-	//Algoritmo de la mochila
-	for(int i = 2; i < N; i++)
+	for(i = 1; i < N; i++)
 	{
-		for(int j = 0; j < ases ; j++)
+		for(int j = 0; j <= ases ; j++)
 		{
 			if(j < p[i])
 				f[i][j] = f[i-1][j];
 				
 			else		
-				f[i][j] = max(f[i-1][j],f[i-1][j - p[i]]+v[i]);
+				f[i][j] = std::max(f[i-1][j],f[i-1][j - p[i]]+v[i]);
 		}
 	}
+
+	//Algoritmo que recupera la combinación óptima de defensas
+	int j = ases;
+	i = N-1;
+
+    	std::list<Defense*>::iterator it = defenses.begin();
+
+	//Colocamos la primera defensa
+	selectedIDs.push_back((*it)->id);
+	j = j - p[0];
 	
-	//Algoritmo ejercicio 2
-	int j = 0;
-	i = 0;
-	
-	while(i != 1 && j != 0)
-	{
+	//Empezamos desde la última defensa
+	it = defenses.end();
+    	it--;
+    	
+	while(i != 0 && j != 0)
+	{	
 		if(f[i][j] != f[i-1][j])
-		{
-			//S <- S U O[i].peso
-			j = j - O[i].peso
+		{					
+			selectedIDs.push_back((*it)->id);
+			j = j - p[i];					
 		}
 		i = i - 1;
+		it--;
 	}
 	
-	if(O[i].peso <= j)
-		//S <- S U O[1]
+	if(p[i] <= j)
+		selectedIDs.push_back((*it)->id);
+
+	/*	
+	unsigned int cost = 0;
+    	std::list<Defense*>::iterator it = defenses.begin();
+ 	while(it != defenses.end()) {
+		if(cost + (*it)->cost <= ases) {
+		    selectedIDs.push_back((*it)->id);
+		    cost += (*it)->cost;
+		}
+		++it;
+    	}	
+		
+	*/
+	
 }
+
